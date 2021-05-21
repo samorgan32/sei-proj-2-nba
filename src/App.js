5470e8463e387a9cf409733ae83348fbb8a15d83
samorgan32 <samorgan32@gmail.com>
2021-05-21 13:22:52 -0500

    clear player comparison on click added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Route } from 'react-router-dom'
 import Search from './components/Search'
 import PlayerComparison from './components/PlayerComparison'
@@ -13,12 +13,27 @@ function App() {
   const [players, setPlayers] = useState(data.data)
   const [activePlayers, setActivePlayers] = useState([])
 
+  let clearPlayers = () => {
+    setActivePlayers([])
+  }
+
+  useEffect(() => {
+
+  }, [])
+
   return (
+
     <div>
-      <Search players={players} setPlayers={setPlayers} activePlayers={activePlayers} setActivePlayers={setActivePlayers} />
-      <PlayerList players={players} setPlayers={setPlayers} activePlayers={activePlayers} setActivePlayers={setActivePlayers} />
-      <Route path="/player-comparison/" render={(routerProps) => <PlayerComparison activePlayers={activePlayers} setActivePlayers={setActivePlayers} match={routerProps.match} />} />
+      <div>
+        <button onClick={clearPlayers}>Clear Players</button>
+      </div>
+      <div>
+        <Search players={players} setPlayers={setPlayers} activePlayers={activePlayers} setActivePlayers={setActivePlayers} />
+        <PlayerList players={players} setPlayers={setPlayers} activePlayers={activePlayers} setActivePlayers={setActivePlayers} />
+        <Route path="/player-comparison/" render={(routerProps) => <PlayerComparison activePlayers={activePlayers} setActivePlayers={setActivePlayers} match={routerProps.match} />} />
+      </div>
     </div>
+
   );
 }
 
