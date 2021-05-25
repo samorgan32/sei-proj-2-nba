56ac81fcd77be419bde00b8f13e21be8a1a556e5
samorgan32 <samorgan32@gmail.com>
2021-05-25 14:22:46 -0500

    player comparison displays alternative message when no stats are available

--- src/components/PlayerComparison.js
+++ src/components/PlayerComparison.js
@@ -2,32 +2,41 @@ import React from 'react';
 
 const PlayerComparison = ({ playerCompare, setPlayerCompare }) => {
     if (!playerCompare) {
-        return <p></p>
+        return null
     }
 
     return (
         <div className='player-comparison'>
-            <div>
-                <h2>{playerCompare.first_name} {playerCompare.last_name}</h2>
 
-                {playerCompare.team && <h3>{playerCompare.team.full_name} </h3>}
-                <h3>{playerCompare.position}</h3>
-                <h3>{playerCompare.height_feet}'{playerCompare.height_inches}</h3>
-                <h3>{playerCompare.weight_pounds} lbs.</h3>
-            </div>
-            <div>
-                <p>{playerCompare.season}</p>
-                <p>{playerCompare.pts}</p>
-                <p>{playerCompare.ast}</p>
-                <p>{playerCompare.reb}</p>
-                <p>{playerCompare.stl}</p>
-                <p>{playerCompare.blk}</p>
-                <p>{playerCompare.min}</p>
-                <p>{parseFloat((playerCompare.fg_pct) * 100).toFixed(2)} </p>
-                <p>{parseFloat((playerCompare.fg3_pct) * 100).toFixed(2)} </p>
-                <p>{playerCompare.turnover}</p>
+            {
+                playerCompare.position ?
+                    <div>
+                        <div>
+                            <h2>{playerCompare.first_name} {playerCompare.last_name}</h2>
 
-            </div>
+                            {playerCompare.team && <h3>{playerCompare.team.full_name} </h3>}
+                            <h3>{playerCompare.position}</h3>
+                            <h3>{playerCompare.height_feet}'{playerCompare.height_inches}</h3>
+                            <h3>{playerCompare.weight_pounds} lbs.</h3>
+                        </div>
+                        <div>
+                            <p>{playerCompare.season}</p>
+                            <p>{playerCompare.pts}</p>
+                            <p>{playerCompare.ast}</p>
+                            <p>{playerCompare.reb}</p>
+                            <p>{playerCompare.stl}</p>
+                            <p>{playerCompare.blk}</p>
+                            <p>{playerCompare.min}</p>
+                            <p>{parseFloat((playerCompare.fg_pct) * 100).toFixed(2)} </p>
+                            <p>{parseFloat((playerCompare.fg3_pct) * 100).toFixed(2)} </p>
+                            <p>{playerCompare.turnover}</p>
+
+                        </div>
+                    </div>
+
+
+                    : <h3>No stats available for {playerCompare.first_name} {playerCompare.last_name}.  Please select another player.</h3>
+            }
 
         </div>
     );
